3888b845d7d6d96233d714f8d375dd9a50b11c74
Priyansh Sourav <ipriyansh@users.noreply.github.com>
2024-11-20 22:17:07 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import PriyanshPortfolio from './PriyanshPortfolio';
+
+function App() {
+  return <PriyanshPortfolio />;
+}
+
+export default App;
